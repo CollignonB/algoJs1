29b474c92d5f2fb07b840be9fa399f6e2b41e5cb
Baptiste COLLIGNON <collignon@CDI>
2020-07-07 10:49:28 +0200

    ex4 done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var hideNum = 15;
-
-var playerGuess = parseInt(prompt("Choisissez un nombre : "), 10);
-console.log(typeof(playerGuess));
-
-while(playerGuess !== hideNum){
-    if(playerGuess > hideNum){
-        playerGuess = parseInt(prompt("Votre choix est plus grand que le nombre caché, Réessayez : "));
-    }else if (playerGuess < hideNum){
-        playerGuess = parseInt(prompt("Votre choix est plus petit que le nombre caché, Réessayez : "));
-    }
+for (var i = 1; i <= 100; i ++){
+    console.log(i + "\n");
 }
-    alert("Vous avez gagné bravo!");
