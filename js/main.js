1e6354133413d2e43e322859fc5b0bf1c6bc4870
Baptiste COLLIGNON <collignon@CDI>
2020-07-07 11:09:56 +0200

    ex7 done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-function poolFilling(largeur, Longueur, profondeur, debit){
-    var poolVolume = largeur * Longueur * profondeur;
-    return poolVolume/debit;
+var rayon = parseInt(prompt("Entrez la valeur du rayon du cercle : "));
+
+function aireCercle (rayon){
+    return (Math.PI * rayon*rayon);
 }
 
-
-console.log("il va faloir " + poolFilling(10,10,5, 20) + " minutes pour remplir la piscine");
+function perimetreCercle (rayon){
+    return (rayon * 2 * Math.PI);
+}
+console.log(rayon);
+console.log("lair du cercle est de : "+ aireCercle(rayon).toFixed(2) + "\nLe perimètre du cercle est de : " 
+                + perimetreCercle(rayon).toFixed(2));
