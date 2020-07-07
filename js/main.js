9b06b872d1ee709b466083fd63d2c2a03df3226d
Baptiste COLLIGNON <collignon@CDI>
2020-07-07 11:23:08 +0200

    ex8 done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-var rayon = parseInt(prompt("Entrez la valeur du rayon du cercle : "));
-
-function aireCercle (rayon){
-    return (Math.PI * rayon*rayon);
+var chaine = "";
+for (var i =1; i < 6; i++){
+        chaine += "*"
+        console.log(chaine);
 }
-
-function perimetreCercle (rayon){
-    return (rayon * 2 * Math.PI);
-}
-console.log(rayon);
-console.log("lair du cercle est de : "+ aireCercle(rayon).toFixed(2) + "\nLe perimètre du cercle est de : " 
-                + perimetreCercle(rayon).toFixed(2));
