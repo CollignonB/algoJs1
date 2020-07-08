2275df3b571f822e5f247c65ea469dab280a4087
Baptiste COLLIGNON <collignon@CDI>
2020-07-08 09:13:54 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-var hideNum = 15;
-
-var playerGuess = parseInt(prompt("Choisissez un nombre : "), 10);
-console.log(typeof(playerGuess));
-
-while(playerGuess !== hideNum){
-    if(playerGuess > hideNum){
-        playerGuess = parseInt(prompt("Votre choix est plus grand que le nombre caché, Réessayez : "));
-    }else if (playerGuess < hideNum){
-        playerGuess = parseInt(prompt("Votre choix est plus petit que le nombre caché, Réessayez : "));
-    }
-}
-    alert("Vous avez gagné bravo!");
+// regarder le code dans les autres branches 
